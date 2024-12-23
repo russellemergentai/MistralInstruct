1217335ec681685ac603320556b3df1f8a03ab24
Russell <52071229+russellemergentai@users.noreply.github.com>
2024-12-23 16:16:50 +0000

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@
 
    @app.route("/")
    def hello():
-       return "Hello, Cloud Run!"
+       return "Hello, Cloud Run"
 
    if __name__ == "__main__":
        app.run(host="0.0.0.0", port=8080)

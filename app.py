79ec70367d51b3740459183d4aeabee03eb82e8d
Russell <52071229+russellemergentai@users.noreply.github.com>
2024-12-28 13:49:42 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-# app.py
 from flask import Flask
+
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def hello():
-    return "Hello, Cloud Run"
+    return 'Hello from Cloud Run!\n'
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080)
+if __name__ == '__main__':
+    app.run(debug=False, host='0.0.0.0', port=8080)
